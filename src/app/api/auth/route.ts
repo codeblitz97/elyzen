31ce97fccfba7bde8bb99900cb0c552fe3eaaa28
Mohtasim Alam <94981761+sulvii@users.noreply.github.com>
2026-07-24 18:22:47 +0600

    Update API routes

--- src/app/api/auth/route.ts
+++
@@ -1,9 +0,0 @@
-import { getServerSession } from 'next-auth';
-import { NextRequest, NextResponse } from 'next/server';
-import { authOptions } from './[...nextauth]/route';
-
-export async function GET(req: NextRequest) {
-  const session = await getServerSession(authOptions);
-  console.log(session);
-  return NextResponse.json({ id: 1 });
-}
